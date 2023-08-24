d472d841b040b4ead67cf75eaa0d32d5a0041c51
singhluvkush286 <singhlavkush286@gmail.com>
2023-08-25 00:50:51 +0530

    Java Basics Program

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d/20a13b04b342001e1ec8969b01400c2a
@@ -0,0 +1,17 @@
+package com.tns.scanner;
+
+public class Calculation {
+  public void cls(Person p)
+  {
+	  if(p.getIncome()<=16000) {
+		  p.setTax(0);
+	  }
+	  else if(p.getIncome()>16000 && p.getIncome()<=50000) {
+		  p.setTax(5);
+	  }
+	  else
+	  {
+		  p.setTax(10);
+	  }
+  }
+}
---
+++ JavaCore/src/com/Demo2.java
@@ -0,0 +1,43 @@
+class Person {
+    private String name;
+    private int age;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age cannot be negative.");
+        }
+    }
+}
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        
+        person.setName("Alice");
+        person.setAge(30);
+        
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        
+        // Trying to set a negative age
+        person.setAge(-5);
+    }
+}
